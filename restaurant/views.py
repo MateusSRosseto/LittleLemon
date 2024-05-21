c7958c7fd73709c748e8a5a86de845484ecd8e0b
MateusSRosseto <mateus.schiavo@hotmail.com>
2024-05-21 04:05:22 -0300

    Modifying the views to require authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -13,10 +13,12 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
