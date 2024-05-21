fa2c882b158b113cf8d29d7c9e7bc23af1ce54a4
MateusSRosseto <mateus.schiavo@hotmail.com>
2024-05-21 02:37:49 -0300

    Setting up user authentication via tokens using DRF and Djoser

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Little Lemon website
 Project of a website for a fictitious restaurant called Little Lemon.
 Created using the web framework Django.
+
+Available endpoints:
+1. Django's default admin site
+2. 'restaurant'
+3. 'restaurant/menu'
+4. 'restaurant/menu/<int:pk>'
+5. 'restaurant/booking/' and its variations defined by the DefaultRouter class
+6. 'obtain-auth-token'
+7. 'auth/' and its variations defined by Djoser for default endpoints for user authentication
--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +141,26 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # APPEND_SLASH = False
+
+# Djoser configuration:
+
+DJOSER = {"USER_ID_FIELD": "username"}
+
+# DRF configuration:
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_FILTER_BACKENDS" : [
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE" : 10,
+}
